0a72b3e3789dce72aeb9c6e94f60036754b3386c
Drake <drakedemuyt@gmail.com>
2020-06-27 13:16:00 -0400

    Addint About me text

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -10,13 +10,13 @@ function About() {
       <Container style={{ marginTop: 30 }}>
         <Row>
           <Col size="md-12">
-          <h1 className="border-bottom"> About Me</h1>
+          <h1 className="border-bottom">About Me</h1>
           </Col>
         </Row>
         <Row>
           <Col size="md-12">
-          <img className="float-left col-md-4 " src="../../public/drakedemuytprofile.jpg" max-width="200px" height="100%" ></img>
-          <div className="text-wrap col-md-8">Hey, I'm Drake! I was born and raised in a small town in Indiana and went to the University of Saint Francis in Fort Wayne, IN. My degree is a Bachelor of Arts in Communication, but I am going back to school with George Washington University's Full-Stack Coding Bootcamp. So far, I am knowledgable in HTML, CSS, and JavaScript. My goals are to work hard in this bootcamp, develop my skills to the utmost ability, and show future employers how dedicated I am to this switch in career paths.</div>
+          <img className="float-left col-sm-4 " src="/img/drakedemuytprofile.jpg"  ></img>
+          <div className="text-wrap col-sm-8">Hey, I'm Drake! I was born and raised in a small town in Indiana and went to the University of Saint Francis in Fort Wayne, IN. My degree is a Bachelor of Arts in Communication, but I am going back to school with George Washington University's Full-Stack Coding Bootcamp. After six months, I am knowledgable in HTML, CSS, JavaScript, MySQL, Progressive Working Applications, and MERN (MongoDB, Expressm, React,and Node.js). My goals are to work hard in this bootcamp, develop my skills to the utmost ability, and show future employers how dedicated I am to this switch in career paths.</div>
           </Col>
         </Row>
       </Container>
