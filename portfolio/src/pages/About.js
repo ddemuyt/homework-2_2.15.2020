d2def7c49aa6684354551345a4f95a9868ddaa66
Drake <drakedemuyt@gmail.com>
2020-06-27 16:34:41 -0400

    Adding email form

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -15,7 +15,7 @@ function About() {
         </Row>
         <Row>
           <Col size="md-12">
-          <img className="float-left col-sm-4 " src="/img/drakedemuytprofile.jpg"  ></img>
+          <img alt="Drake DeMuyt profile" className="float-left col-sm-4 " src="/img/drakedemuytprofile.jpg"  ></img>
           <div className="text-wrap col-sm-8">Hey, I'm Drake! I was born and raised in a small town in Indiana and went to the University of Saint Francis in Fort Wayne, IN. My degree is a Bachelor of Arts in Communication, but I am going back to school with George Washington University's Full-Stack Coding Bootcamp. After six months, I am knowledgable in HTML, CSS, JavaScript, MySQL, Progressive Working Applications, and MERN (MongoDB, Expressm, React,and Node.js). My goals are to work hard in this bootcamp, develop my skills to the utmost ability, and show future employers how dedicated I am to this switch in career paths.</div>
           </Col>
         </Row>
--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -9,23 +9,36 @@ function Contact() {
     <div>
     
       <Container style={{ marginTop: 30 }}>
-        {/* <Row>
+        <Row>
           <Col size="md-12">
           <h1 className="border-bottom">Contact Me</h1>
+          <form action="mailto:drakedemuyt@gmail.com" method="post" enctype="text/plain">
+            <div className="form-group">
+              <label for="exampleInputName1">Name</label>
+              <input type="name" className="form-control" id="exampleInputName1" placeholder="John Doe"/>
+            <div className="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" aria-describedby="emailHelp"/>
+            </div>
+            <div className="form-group">
+              <label for="exampleInputMessage1">Message</label>
+              <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            </div>
+            
+            <input className="btn btn-color" type="submit" value="Submit"/>
+          </form>
+
           </Col>
-        </Row> */}
+        </Row>
         <Row>
           <Col size="md-6">
               
-              <a href="https://github.com/ddemuyt"><img src={"/img/github.png"} style={{height: 200}}/></a>
+              <a  href="https://github.com/ddemuyt"><img alt="GitHub Link" src={"/img/github.png"} style={{height: 200}}/></a>
           </Col>
           <Col size="md-6">
-              <a href="https://www.linkedin.com/in/drake-demuyt/"><img style={{height: 175}} src="https://media-exp1.licdn.com/dms/image/C4D0BAQGyOWvr4W0Pow/company-logo_200_200/0?e=2159024400&v=beta&t=itrwplyUUwPAVxqxN8THySQds9p401UaOtZIurSBVnA"/></a>
+              <a href="https://www.linkedin.com/in/drake-demuyt/"><img alt="LinkedIn Link" style={{height: 175}} src="https://media-exp1.licdn.com/dms/image/C4D0BAQGyOWvr4W0Pow/company-logo_200_200/0?e=2159024400&v=beta&t=itrwplyUUwPAVxqxN8THySQds9p401UaOtZIurSBVnA"/></a>
           </Col>
-          {/* <Col size="md-12">
-              <img src="https://media-exp1.licdn.com/dms/image/C4D0BAQGyOWvr4W0Pow/company-logo_200_200/0?e=2159024400&v=beta&t=itrwplyUUwPAVxqxN8THySQds9p401UaOtZIurSBVnA"/>
-              <a href="https://www.linkedin.com/in/drake-demuyt/">LinkedIn</a>
-          </Col> */}
         </Row>
       </Container>
     </div>
